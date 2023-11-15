d48da793a4fd369f3ae27ffe523306f4316033f7
LKA102 <stefano.hernandez@unmsm.edu.pe>
2023-11-15 14:28:17 -0500

    Corrigiendo stored procedure SET CATEGORIA

--- Aplicacion/BaseDatos/CREATE-STORED-PROCEDURES.sql
+++ Aplicacion/BaseDatos/CREATE-STORED-PROCEDURES.sql
@@ -2,33 +2,35 @@ CREATE OR REPLACE PROCEDURE SCHEMA_TIENDA.SP_SET_CATEGORIA (
         IN p_ID_CATEGORIA SMALLINT,
         IN p_DESCRIPCION VARCHAR(25)
     ) 
+SPECIFIC SP_SET_CATEGORIA
+LANGUAGE SQL 
 BEGIN
     DECLARE v_row_count INT;
-    
-    -- Intenta actualizar la categoría existente
+
+--Intenta actualizar la categoría existente
     UPDATE SCHEMA_TIENDA.TBL_CATEGORIAS
     SET DESCRIPCION = p_DESCRIPCION
     WHERE ID_CATEGORIA = p_ID_CATEGORIA;
-    
-    -- Obtiene el número de filas afectadas por la actualización
-    SET v_row_count = ROW_COUNT;
-    
-    -- Si no se actualizó ninguna fila, entonces intenta insertar una nueva categoría
+
+--Obtiene el número de filas afectadas por la actualización
+    GET DIAGNOSTICS v_row_count = ROW_COUNT;
+
+--Si no se actualizó ninguna fila, entonces intenta insertar una nueva categoría
     IF v_row_count = 0 THEN
         INSERT INTO SCHEMA_TIENDA.TBL_CATEGORIAS (DESCRIPCION)
-        VALUES (p_DESCRIPCION);
-        
-        -- Obtiene el número de filas afectadas por la inserción
-        SET v_row_count = ROW_COUNT;
-        
-        -- Si no se insertó ninguna fila, lanza una excepción
+        VALUES(p_DESCRIPCION);
+
+--Obtiene el número de filas afectadas por la inserción
+        GET DIAGNOSTICS v_row_count = ROW_COUNT;
+
+--Si no se insertó ninguna fila, lanza una excepción
         IF v_row_count = 0 THEN
         	ROLLBACK;
-            SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'No se pudo actualizar ni insertar la categoría.';
-        END IF;
-    END IF;
-    
-    COMMIT;
+SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'No se pudo actualizar ni insertar la categoría.';
+END IF;
+END IF;
+
+COMMIT;
 END
 
 CREATE OR REPLACE PROCEDURE SCHEMA_TIENDA.SP_GET_CATEGORIA ()
