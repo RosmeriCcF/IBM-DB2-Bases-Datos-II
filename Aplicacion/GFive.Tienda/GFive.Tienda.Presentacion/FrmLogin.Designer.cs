11dcdd811d773d39d445546347d02730ddb38a46
LKA102 <stefano.hernandez@unmsm.edu.pe>
2023-11-15 23:11:41 -0500

    Arreglando formulario de LogIn

--- Aplicacion/GFive.Tienda/GFive.Tienda.Presentacion/FrmLogin.Designer.cs
+++ Aplicacion/GFive.Tienda/GFive.Tienda.Presentacion/FrmLogin.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmLogin));
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.panel1 = new System.Windows.Forms.Panel();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
@@ -322,6 +323,7 @@
             this.ClientSize = new System.Drawing.Size(726, 530);
             this.Controls.Add(this.tableLayoutPanel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FrmLogin";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FrmLogin";
